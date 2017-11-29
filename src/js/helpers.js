569c714b98272eaf2ca0ed7caaaff319b8c53f19
Alan Gabriel Dawidowicz <alan.dawidowicz@mediamonks.com>
2017-11-29 18:37:20 -0300

    Create helpers file

---
+++ src/js/helpers.js
@@ -0,0 +1,46 @@
+import { json } from './api';
+
+export const transform = callback => {
+    json('./from.json', function(data) {
+      var schema = {};
+      schema.data = {};
+  
+      for (var propName in data) {
+        schema.data[propName] = data[propName];
+  
+        if (propName === 'properties') {
+          schema.data[propName] = [];
+  
+          for (var subPropName in data[propName]) {
+            var subSchema = {};
+  
+            subSchema.name = subPropName;
+  
+            for (var subSubPropName in data[propName][subPropName]) {
+              subSchema[subSubPropName] = data[propName][subPropName][subSubPropName];
+  
+              if (subSubPropName === 'properties') {
+                subSchema[subSubPropName] = [];
+  
+                for (var subSubSubPropName in data[propName][subPropName][subSubPropName]) {
+                  var obj = {};
+  
+                  subSchema.name = subSubSubPropName;
+  
+                  for (var subSubSubSubPropName in data[propName][subPropName][subSubPropName][subSubSubPropName]) {
+                    obj[subSubSubSubPropName] = data[propName][subPropName][subSubPropName][subSubSubPropName][subSubSubSubPropName];
+                  }
+  
+                  subSchema[subSubPropName].push(obj);
+                }
+              }
+            }
+  
+            schema.data[propName].push(subSchema);
+          }
+        }
+      }
+  
+      callback(schema);
+    });
+  }
